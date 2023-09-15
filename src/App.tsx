feddc8e232e936cd81f222d4e2cc8b5f70c4454b
DeCaelo <ludovic.cleuet@gmail.com>
2023-09-15 15:11:52 +0200

    cva power

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Hero from "./components/Hero";
-import { Button } from "./components/Buttons";
+import { Button, ButtonProps } from "./components/Buttons";
+
+const sizes: ButtonProps["size"][] = ["sm", "md", "lg", "xl", "xl"];
+const buttonOptions: ButtonProps["intent"][] = ["primary", "outline", "ghost"];
 
 function App() {
   return (
@@ -8,7 +11,63 @@ function App() {
         <Hero />
         <div className="mx-auto">
           <div className="grid place-items-center gap-12 mt-16">
-            <Button>Learn more</Button>
+            {buttonOptions.map((option) => (
+              <div
+                className="flex gap-6 justify-center items-center"
+                key={option}
+              >
+                <div className="grid gap-2">
+                  <h2 className="text-xl tracking-wide font-bold uppercase">
+                    {option} Buttons
+                  </h2>
+                  <div className="flex items-center justify-center gap-4 flex-wrap">
+                    {sizes.map((size, index) => (
+                      <Button
+                        size={size}
+                        intent={option}
+                        disabled={index === 4}
+                        key={size}
+                      >
+                        <span>Learn more</span>
+                        <span>&rarr;</span>
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+                <div className="grid gap-2">
+                  <h2 className="text-xl tracking-wide font-bold uppercase">
+                    {option} Icon
+                  </h2>
+                  <div className="flex items-center justify-center gap-4 flex-wrap">
+                    {sizes.map((size, index) => (
+                      <Button
+                        size={size}
+                        intent={option}
+                        btnType={"icon"}
+                        disabled={index === 4}
+                        key={size}
+                      >
+                        <svg
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 12h14m-7-7v14"
+                          />
+                        </svg>
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,33 +1,79 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 import { cva, type VariantProps } from "class-variance-authority";
 
-const button = cva("button", {
-  variants: {
-    intent: {
-      primary: [
-        "bg-blue-500",
-        "text-white",
-        "border-transparent",
-        "hover:bg-blue-600",
-      ],
-      secondary: [
-        "bg-white",
-        "text-gray-800",
-        "border-gray-400",
-        "hover:bg-gray-100",
-      ],
+const button = cva(
+  [
+    "leading-none",
+    "flex",
+    "gap-2",
+    "items-center",
+    "justify-center",
+    "focus:outline-none",
+    "focus-visible:ring-4",
+    "transition-all",
+    "rounded-lg",
+    "ring-theme-300",
+    "disabled:cursor-not-allowed",
+  ],
+  {
+    variants: {
+      intent: {
+        primary: [
+          "bg-theme-600",
+          "text-theme-50",
+          "hover:bg-theme-500",
+          "active:bg-theme-700",
+          "focus-visible:bg-theme-600",
+          "disabled:bg-theme-50",
+          "disabled:bg-theme-200",
+          "disabled:text-theme-400",
+        ],
+        outline: [
+          "bg-theme-base",
+          "text-theme-600",
+          "border-2",
+          "border-theme-600",
+          "hover:bg-theme-100",
+          "active:bg-theme-200",
+          "disabled:bg-theme-base",
+          "disabled:border-theme-200",
+          "disabled:text-theme-400",
+        ],
+        ghost: [
+          "bg-theme-base",
+          "text-theme-600",
+          "hover:bg-theme-50",
+          "active:bg-theme-100",
+          "disabled:bg-theme-base",
+          "disabled:border-theme-200",
+          "disabled:text-theme-400",
+        ],
+      },
+      size: {
+        sm: ["text-sm", "py-1.5", "min-h-9", "px-3"],
+        md: ["text-sm", "py-2", "min-h-10", "px-5"],
+        lg: ["text-md", "py-2.5", "min-h-11", "px-5"],
+        xl: ["text-md", "py-3", "min-h-12", "px-6"],
+      },
+      btnType: {
+        button: "",
+        icon: ["p-0", "rounded-full"],
+      },
     },
-    size: {
-      small: ["text-sm", "py-1", "px-2"],
-      medium: ["text-base", "py-2", "px-4"],
+    compoundVariants: [
+      { btnType: "icon", size: "sm", class: "h-10 w-10" },
+      { btnType: "icon", size: "md", class: "h-11 w-11" },
+      { btnType: "icon", size: "lg", class: "h-12 w-12" },
+      { btnType: "icon", size: "xl", class: "h-[52px] w-[52px]" },
+    ],
+    defaultVariants: {
+      intent: "primary",
+      size: "md",
+      btnType: "button",
     },
-  },
-  compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
-  defaultVariants: {
-    intent: "primary",
-    size: "medium",
-  },
-});
+  }
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -37,5 +83,11 @@ export const Button: React.FC<ButtonProps> = ({
   className,
   intent,
   size,
+  btnType,
   ...props
-}) => <button className={button({ intent, size, className })} {...props} />;
+}) => (
+  <button
+    className={twMerge(button({ intent, size, btnType, className }))}
+    {...props}
+  />
+);
